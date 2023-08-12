2701841675b4e929edcf23d8ba9b98736e82fb27
Pavan Prakash <pavan@morethanmetrics.com>
2023-08-12 22:45:28 +0530

    add logo

---
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -0,0 +1,16 @@
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import type { NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      kindeAuth: any;
+    };
+  }
+) {
+  const endpoint = params.kindeAuth;
+  return handleAuth(request, endpoint);
+}
